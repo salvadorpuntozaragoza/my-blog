27bee30e8c2753b90f89555e9191d15023d4180e
Salvador Zaragoza <salvador.zaragoza@uabc.edu.mx>
2022-07-01 17:02:24 -0700

    First version

---
+++ app/routes/index.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <div className="flex flex-col bg-red-400 w-100 h-screen items-center justify-center">
+      <a className="hover:underline hover:decoration-blue-600 hover:text-blue-600" href="https://www.twitter.com/Salvador_ZG" target={"_parent"}>
+        <span className="font-medium text-lg">Follow me on twitter!!</span>
+      </a>
+    </div>
+  );
+}
